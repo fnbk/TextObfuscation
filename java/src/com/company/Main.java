fc5ed636221ab6847a8c2a3499b47bf48eee2fe4
Florian Boehmak <florian@fnbk.cc>
2020-05-30 16:50:03 +0200

    remove hello world

--- java/src/com/company/Main.java
+++ java/src/com/company/Main.java
@@ -3,7 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
         var text = "Always look on the bright side of life!";
 
         var manager = new TextManager();
